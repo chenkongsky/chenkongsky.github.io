db6dc6e31a014039adf71a3dc2acf486217d0d4d
ken <ken605.sky@gmail.com>
2020-11-26 10:46:03 +0800

    bbb

--- sky/aaaa.js
+++ sky/aaaa.js
@@ -1,3 +1,4 @@
-document.getElementsByTagName("body").onclick=function(){
+document.getElementsByTagName("body")[0].onclick=function(){
     console.log("body 点击")
 }
+console.log(document.getElementsByTagName("body"),'111')
